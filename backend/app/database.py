a64c70e308fe8eb3ae88c8802a9f941b4f6ff93f
AnjaliRawatAR <anjalivipulrawat@gmail.com>
2025-03-29 12:22:51 +0530

    minor changes

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./users.db"  # Change to your database URL if using MySQL/PostgreSQL
+DATABASE_URL = "sqlite:///./users.db"  
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})  # SQLite needs this
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})  
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
