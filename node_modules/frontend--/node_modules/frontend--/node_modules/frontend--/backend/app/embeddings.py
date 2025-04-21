5fabb89b82241e80ea83b8a9d78f309c82a63b40
Akanksha530 <125267386+Akanksha530@users.noreply.github.com>
2025-04-21 11:07:53 +0530

    changes in frontend

--- node_modules/frontend--/node_modules/frontend--/node_modules/frontend--/backend/app/embeddings.py
+++
@@ -1,8 +0,0 @@
-# from sentence_transformers import SentenceTransformer
-
-# # Load transformer model
-# model = SentenceTransformer("all-MiniLM-L6-v2")
-
-# def get_embedding(text):
-#     """Convert text into a vector embedding"""
-#     return model.encode(text, convert_to_numpy=True)
